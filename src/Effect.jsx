861d6990311014603887a40711dbc0aa0689dbbb
balram kamble <balram.kamble@gmail.com>
2026-03-25 20:56:19 +0530

    updated code

--- src/Effect.jsx
+++ src/Effect.jsx
@@ -1,9 +1,19 @@
+import { useEffect, useState } from "react"
 export default function Effect(){
-    const [content, setContent] = useState('Post')
+    const [content, setContent] = useState('Post');
+    useEffect(()=>{
+        console.log('Content mounted');
+    }, [])
+     useEffect(()=>{
+        console.log('Content updated');
+        return()=>{
+            console.log('Content unmounted');
+        }
+    })
     return(
         <div>
             <button className="btn btn-primary" onClick={ ()=> setContent('Post')}>Post</button>
-            <button className="btn btn-primary" onClick={ ()=> setContent('Userr')}>User</button>
+            <button className="btn btn-primary" onClick={ ()=> setContent('User')}>User</button>
             <button className="btn btn-primary" onClick={ ()=> setContent('Comment')}>Comments</button>
            {content}
         </div>   
